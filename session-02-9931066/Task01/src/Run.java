11b29b5bbcda69768c6d2d0401b58c716a623a23
roninix <airavanimanesh@gmail.com>
2021-03-12 13:55:15 +0330

    L2-T1

---
+++ session-02-9931066/Task01/src/Run.java
@@ -0,0 +1,19 @@
+public class Run {
+    public static void main(String[] args){
+        Student std1 = new Student("Ehsan","Edalat","9031066");
+        Student std2 = new Student("Amirparsa","Salmankhah","9831034");
+        Student std3 = new Student("Amirhossein","Iravanimanesh","9931066");
+
+        std1.print();
+        std1.setGrade(15);
+        std1.print();
+
+        std2.print();
+        std2.setGrade(11);
+        std2.print();
+
+        std3.print();
+        std3.setFirstName("HamidReza");
+        std3.print();
+    }
+}
