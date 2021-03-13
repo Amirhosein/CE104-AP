13ed41f11b5fb7322a2c2dd5fc3ea43b95337d35
roninix <airavanimanesh@gmail.com>
2021-03-13 08:09:58 +0330

    Adding docs L2 ...

--- session-02-9931066/Task03/src/Student.java
+++ session-02-9931066/Task03/src/Student.java
@@ -1,3 +1,6 @@
+/**
+ * A class definition for student
+ */
 public class Student{
     private String firstName;
 
@@ -7,7 +10,12 @@ public class Student{
 
     private int grade;
 
-
+    /** * Create a new student with a given name and ID number. *
+     *
+     * @param fName first name of students
+     * @param lName last name of students
+     * @param sID ID of students
+     */
     public Student( String fName, String lName, String sID){
         firstName = fName;
         lastName = lName;
@@ -15,39 +23,69 @@ public class Student{
         grade = 0;
     }
 
-
+    /**
+     * get student's first name
+     * @return firstName string
+     */
     public String getFirstName(){
         return firstName;
     }
 
+    /**
+     * @param fName set first name of student
+     */
     public void setFirstName(String fName){
         firstName = fName;
     }
 
+    /**
+     * get student's last name
+     * @return lastName string
+     */
     public String getLastName(){
         return lastName;
     }
 
+    /**
+     * @param lName set last name of student
+     */
     public void setLastName(String lName){
         lastName = lName;
     }
 
+    /**
+     * get student's ID
+     * @return ID string
+     */
     public String getID(){
         return ID;
     }
 
+    /**
+     * @param sID set ID of student
+     */
     public void setID(String sID){
         ID = sID;
     }
 
+    /**
+     * get student's grade
+     * @return grade int
+     */
     public int getGrade(){
         return grade;
     }
 
+    /**
+     * @param Grade set grade of student
+     */
     public void setGrade(int Grade){
         grade = Grade;
     }
 
+    /**
+     * print all information of a student.
+     */
     public void print(){
         System.out.println("Name: " + firstName + " " + lastName + "\n" + "Student ID: " + ID + "\n" + "Grade: " + grade + "\n\n");
     }
