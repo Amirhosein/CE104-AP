68b063f3d564a87702fab4fa3c5840335635ccb5
roninix <airavanimanesh@gmail.com>
2021-04-06 16:18:22 +0430

    M2-T4 fin & M2-T1 update

--- Series 2/Task 4/src/Main.java
+++ Series 2/Task 4/src/Main.java
@@ -1,39 +1,190 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Main {
-    public static void main(String[] args) {
-        person p1 = new person("Ted", "Mosby", "123456");
-        p1.express();
-        p1.express("Happy");
-        p1.print();
+class LinkedList {
+    private static LinkedList list = new LinkedList();
+    Node head = null;
+
+    static class Node {
+        int number;
+        Node next;
+
+        Node(int n) {
+            number = n;
+            next = null;
+        }
     }
 
-    static class person {
-        private String firstName;
-        private String lastName;
-        private String id;
+    public static void add(int k) {
+        Node new_node = new Node(k);
+        new_node.next = null;
 
-        public person(String firstName, String lastName, String id) {
-            this.firstName = firstName;
-            this.lastName = lastName;
-            this.id = id;
+        if (list.head == null) {
+            list.head = new_node;
+        }
+        else {
+            Node last = list.head;
+            while (last.next != null) {
+                last = last.next;
+            }
+
+            last.next = new_node;
+        }
+    }
+
+    public static void addFirst(int k) {
+        Node new_node = new Node(k);
+        new_node.next = null;
+
+        new_node.next = list.head;
+        list.head = new_node;
+
+//        Node first = list.head;
+//
+//        while (first.next != null) {
+//            first = first.next;
+//        }
+//
+//        first.next = new_node;
+    }
+
+    public static void findMiddle() {
+        int counter = 0;
+        Node node = list.head;
+
+        while (node != null && node.next != null) {
+            counter++;
+            node = node.next;
         }
 
-        public void express() {
-            System.out.println("I feel neutral");
+        if (counter == 0) {
+            System.out.println(-1);
+        } else {
+            node = list.head;
+            for (int i = 0; i < counter/2; i++) {
+                node = node.next;
+            }
+            System.out.println(node.number);
+        }
+    }
+
+    public static void removeMiddle() {
+        int counter = 0;
+        Node node = list.head;
+
+        while (node.next != null) {
+            counter++;
+            node = node.next;
         }
 
-        public int express(String state) {
-            System.out.println("I feel " + state + " today");
-            return 0;
+        if (counter > 0) {
+            node = list.head;
+            for (int i = 0; i < counter/2 - 1; i++) {
+                node = node.next;
+            }
+            node.next = node.next.next;
+        }
+    }
+
+    public static void print() {
+        Node node = list.head;
+        System.out.print(node.number + " ");
+
+        while (node.next != null) {
+            node = node.next;
+            System.out.print(node.number + " ");
         }
 
-        public void print() {
-            System.out.println("person{" +
-                    "name='" + firstName + '\'' +
-                    ", lname='" + lastName + '\'' +
-                    ", id=" + id +
-                    '}');
+        System.out.println();
+    }
+
+    public static void contains(int k) {
+        boolean contains = false;
+        int place = 0;
+        Node node = list.head;
+
+        if (node.number == k){
+            contains = true;
+            System.out.println(place);
+        }
+
+        while (node.next != null) {
+            place++;
+            node = node.next;
+            if (node.number == k){
+                contains = true;
+                System.out.println(place);
+            }
+        }
+
+        if (!contains) {
+            System.out.println(-1);
+        }
+    }
+
+    public static void removeIndex(int i) {
+        int place = 0;
+        Node node = list.head;
+
+        if (i == 0){
+            list.head = node.next;
+            return;
+        }
+
+        while (node.next != null) {
+            place++;
+            node = node.next;
+            if (place == i-1){
+                node.next = node.next.next;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String command = "";
+        while (!command.equals("finish")) {
+            command = scanner.nextLine();
+
+            if (command.matches("add \\d+")) {
+                Pattern p = Pattern.compile("add (\\d+)");
+                Matcher m = p.matcher(command);
+                m.find();
+                int number = Integer.parseInt(m.group(1));
+                add(number);
+            }
+            else if (command.matches("addFirst \\d+")) {
+                Pattern p = Pattern.compile("addFirst (\\d+)");
+                Matcher m = p.matcher(command);
+                m.find();
+                int number = Integer.parseInt(m.group(1));
+                addFirst(number);
+            }
+            else if (command.matches("findMiddle")) {
+                findMiddle();
+            }
+            else if (command.matches("removeMiddle")) {
+                removeMiddle();
+            }
+            else if (command.matches("print")) {
+                print();
+            }
+            else if (command.matches("contains \\d+")) {
+                Pattern p = Pattern.compile("contains (\\d+)");
+                Matcher m = p.matcher(command);
+                m.find();
+                int number = Integer.parseInt(m.group(1));
+                contains(number);
+            }
+            else if (command.matches("removeIndex \\d+")) {
+                Pattern p = Pattern.compile("removeIndex (\\d+)");
+                Matcher m = p.matcher(command);
+                m.find();
+                int index = Integer.parseInt(m.group(1));
+                removeIndex(index);
+            }
         }
     }
 }
-
