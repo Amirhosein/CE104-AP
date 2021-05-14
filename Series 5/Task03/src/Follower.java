a42c40e4a8bc1aba62c9c0fdb8be54a227d698e1
roninix <airavanimanesh@gmail.com>
2021-05-14 17:36:25 +0430

    M5-T3 update

--- Series 5/Task03/src/Follower.java
+++ Series 5/Task03/src/Follower.java
@@ -1,13 +1,31 @@
-public class Follower {
+/**
+ * The type Follower.
+ */
+public class Follower implements Update {
     private String name;
 
+    /**
+     * Instantiates a new Follower.
+     *
+     * @param name the name
+     */
     public Follower(String name) {
         this.name = name;
     }
 
+    /**
+     * Gets name.
+     *
+     * @return the name
+     */
     public String getName() {
         return name;
     }
 
-    public void update
+    @Override
+    public void update(String announcementType, String title, String content) {
+        System.out.println("User: " + name + "\t\t Type: " +
+                announcementType + "\n" + content + "\n---End" +
+                " of announcement---");
+    }
 }
--- Series 5/Task03/src/Update.java
+++ Series 5/Task03/src/Update.java
@@ -1,3 +1,13 @@
+/**
+ * The interface Update.
+ */
 public interface Update {
+    /**
+     * Update.
+     *
+     * @param announcementType the announcement type
+     * @param title            the title
+     * @param content          the content
+     */
     void update(String announcementType , String title , String content);
 }
