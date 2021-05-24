bae695e01d1d2ed822d4855f04efea8b163b2395
roninix <airavanimanesh@gmail.com>
2021-05-24 21:09:47 +0430

    L7-T1 update

---
+++ session-08-9931066/Task01/src/Client.java
@@ -0,0 +1,38 @@
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        try (Socket client = new Socket("127.0.0.1", 5757)) {
+            System.out.println("Connected to server.");
+            OutputStream out = client.getOutputStream();
+            InputStream in = client.getInputStream();
+            byte[] buffer = new byte[2048];
+            while (true) {
+                int read = in.read(buffer);
+                String income = new String(buffer, 0, read);
+                if (income.equals("over"))
+                    break;
+                String msg = input.nextLine();
+                out.write(msg.getBytes());
+                System.out.println("SENT: " + msg);
+                read = in.read(buffer);
+                income = new String(buffer, 0, read);
+                if (income.equals("over"))
+                    break;
+                System.out.println("RECV: " + income);
+            }
+            System.out.print("All messages sent.\nClosing ... ");
+        } catch (IOException ex) {
+            System.err.println(ex);
+            ex.printStackTrace();
+        }
+        System.out.println("done.");
+    }
+}
---
+++ session-08-9931066/Task01/src/Server.java
@@ -0,0 +1,41 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class Server {
+
+    public static void main(String[] args) {
+        ArrayList<String> strings = new ArrayList<>();
+        try (ServerSocket welcomingSocket = new ServerSocket(5757)) {
+            System.out.print("Server started.\nWaiting for a client ... ");
+            try (Socket connectionSocket = welcomingSocket.accept()) {
+                System.out.println("client accepted!");
+                OutputStream out = connectionSocket.getOutputStream();
+                InputStream in = connectionSocket.getInputStream();
+                byte[] buffer = new byte[2048];
+                for (int i = 0; i < 3; i++) {
+                    int read = in.read(buffer);
+                    String input = new String(buffer, 0, read);
+                    System.out.println("RECV: " + input);
+                    strings.add(input);
+                    String msg = strings.stream().collect(Collectors.joining(" "));
+                    out.write(msg.getBytes());
+                    System.out.println("SENT: " + msg);
+                }
+                out.write("over".getBytes());
+                System.out.println("SENT: " + "over");
+                System.out.print("All messages sent.\nClosing client ... ");
+            } catch (IOException ex) {
+                System.err.println(ex);
+            }
+            System.out.print("done.\nClosing server ... ");
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+        System.out.println("done.");
+    }
+}
